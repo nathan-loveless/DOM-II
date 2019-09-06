ea2817df3c8cb187847d8c00d3ebf97224b677bf
Nathan S. Loveless <nloveles@norwich.edu>
2019-09-06 15:20:23 -0400

    Working on MVP goals

--- js/index.js
+++ js/index.js
@@ -1,1 +1,54 @@
 // Your code goes here
+// mouseover/mouseout events
+const navItems = document.querySelectorAll('.nav a');
+scale = 1.0;
+
+navItems.forEach((element) =>
+{
+    element.addEventListener('mouseover', (event) => 
+    {
+        element.style.backgroundColor = 'blue';
+        element.style.color = 'white';
+    });
+    
+    element.addEventListener('mouseout', (event) =>
+    {
+        element.style.border = 'none';
+        element.style.backgroundColor = 'white';
+        element.style.color = '#212529';
+    });
+});
+
+// keydown/keyup events
+window.addEventListener('keydown', (event) =>
+{
+    const container1 =  document.querySelectorAll('.text-content');
+    container1.forEach((element) => element.style.border = '1px solid black')
+    const container2 =  document.querySelectorAll('.destination');
+    container2.forEach((element) => element.style.border = '1px solid black')
+});
+
+window.addEventListener('keyup', (event) =>
+{
+    const container1 =  document.querySelectorAll('.text-content');
+    container1.forEach((element) => { element.style.border = 'none';})
+    const container2 =  document.querySelectorAll('.destination');
+    container2.forEach((element) => { element.style.border = 'none'; })
+});
+
+// window.addEventListener('wheel', (event) =>
+// {
+//     scale += event.deltaY * 0.001;
+//     bodyItem = document.querySelector('body');
+//     bodyItem.style.transform = 'scale(' + scale + ')'
+// });
+
+let item;
+document.addEventListener('drag', function(event) { }, false);
+document.addEventListener('dragstart', (event) => item = event.target);
+document.addEventListener('drop', (event) => 
+{
+    item.parentNode.removeChild(item);
+    event.target.appendChild(item);
+})
+
